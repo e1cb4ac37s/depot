2483e5ad004c5d7839f561dad1ffe637b700a532
e1cb4 ac37s <e1cb4ac37s@gmail.com>
2020-10-24 18:04:19 +0300

    d1: create a cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_050203) do
+ActiveRecord::Schema.define(version: 2020_10_24_145834) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
